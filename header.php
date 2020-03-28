bcc0d8e30b62a307dd13cbe2b037bdaf6336daba
Sergio2697sm <sergiomartinez2m@gmail.com>
2020-03-28 22:06:43 +0100

    modificado

--- header.php
+++ header.php
@@ -1,51 +1,49 @@
+<?php
+//session_start();
+?>
 <header>
     <!-- Encabezado -->
     <div class="tagDivCabecera">
         <a href="/ProyectoGymArtCopia/index.php"><img src="/ProyectoGymArt/imagenes/logo1.png"></a>
         <nav class="navegacion">
-            <li> <a href="">QUIENES SOMOS</a></li>
-            <li><a href="">CONTACTO</a></li>
-            <li><a href="/ProyectoGymArtCopia/cuotas.php">CUOTAS</a></li>
-            <li>GESTIONAR
-                <ul class="subnavegacion">
-                    <li><a href="/ProyectoGymArtCopia/clientes/verClientes.php">Clientes</a></li>
-                    <li><a href="/ProyectoGymArtCopia/mensualidades/verMensualidades.php">Mensualidades</a></li>
-                    <li><a href=""> Pagos</a></li>
-                </ul>
-            </li>
-            <li><a href="usuarios/registrar_usuario.php">REGISTRARSE</a></li>
-            <div class="botonIniciar">
-                <li><a href="/ProyectoGymArtCopia/usuarios/inicioSesion.php">INICIAR SESIÓN</a></li>
-            </div>
+            <ul>
+                <li> <a href="">QUIENES SOMOS</a></li>
+                <li><a href="">CONTACTO</a></li>
+                <li><a href="/ProyectoGymArtCopia/cuotas.php">CUOTAS</a></li>
+                <li>GESTIONAR
+                    <ul class="subnavegacion">
+                        <li><a href="/ProyectoGymArtCopia/clientes/verClientes.php">Clientes</a></li>
+                        <li><a href="/ProyectoGymArtCopia/mensualidades/verMensualidades.php">Mensualidades</a></li>
+                        <li><a href=""> Pagos</a></li>
+                    </ul>
+                </li>
+                <li><a href="usuarios/registrar_usuario.php">REGISTRARSE</a></li>
+                <div class="botonIniciar">
+                    <li><a href="/ProyectoGymArtCopia/usuarios/inicioSesion.php">INICIAR SESIÓN</a></li>
+                </div>
         </nav>
-        <!-- <div>
-            <?php
-            // session_start();
-            // if ($_SESSION) {
-            // ?>
-                <!-- <div class="Bienvenido">
-            //         <p>Bienvenido, <?php //echo $_SESSION['usuario'] ?></p>
-            //         <nav class="navegacion">
-            //             <li><a href="">CERRAR USUARIO</a></li>
-
-            //             <form action="index.php" method="POST">
-            //                 <input type="submit" value="Cerrar session" name="cerrar-session">
-            //             </form>
-            //             </li>
-
-            //         </nav>
-
-            //     </div> -->
-            <?php
-            // } else {
-            //}
-            // if ($_POST) {
-            //     if (isset($_POST['cerrar-session'])) {
-            //         session_unset();
-            //         header('Location:index.php');
-            //     }
-           // }
-            ?>
-       <!--- </div> -->
     </div>
+    <?php
+    if ($_SESSION) {
+    ?>
+        <div class="cerrarSesion">
+            <p>Bienvenido, <?php echo $_SESSION['usuario']
+                            ?></p>
+            <form action="index.php" method="POST">
+                <input type="submit" value="Cerrar session" name="cerrar-session">
+            </form>
+        </div>
+
+        </li>
+    <?php
+    } else {
+    }
+    if ($_POST) {
+        if (isset($_POST['cerrar-session'])) {
+            session_unset();
+            header('Location:/ProyectoGymArtCopia/index.php');
+        }
+    }
+    ?>
+
 </header>
