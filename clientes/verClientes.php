018861611fe0110356875bec06d64277e37625e8
Sergio2697sm <sergiomartinez2m@gmail.com>
2020-04-01 10:49:36 +0200

    modificado

--- clientes/verClientes.php
+++ clientes/verClientes.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 include '../BBDD/conexionBBDD.php';
 include '../BBDD/peticiones.php';
 ?>
--- index.php
+++ index.php
@@ -8,25 +8,25 @@ To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title>ART GYM</title>
-        <link rel="stylesheet" media=" only screen and (min-device-width : 320px) and (max-device-width : 600px)" href="css/estilos_art_movil.css">
+
+<head>
+    <meta charset="UTF-8">
+    <title>ART GYM</title>
+    <link rel="stylesheet" media=" only screen and (min-device-width : 320px) and (max-device-width : 600px)" href="css/estilos_art_movil.css">
     <link rel="stylesheet" media=" only screen and (min-device-width : 601px) and (max-device-width : 1280px)" href="css/estilos_art_table.css">
     <link rel="stylesheet" media=" only screen and (min-device-width : 1281px) " href="css/estilos_art.css">
     <!-- <link rel="stylesheet" media="screen and (min-width:321px) and (max-width:480px)" href="css/tablet.css"> -->
 
+    <!-- <link rel="stylesheet" href="css/estilos_art.css"> -->
 
+</head>
 
+<body>
+    <?php
+    include 'header.php';
+    include 'TextoInicial.php';
+    include 'footer.php';
+    ?>
+</body>
 
-        <!-- <link rel="stylesheet" href="css/estilos_art.css"> -->
-
-    </head>
-    <body>
-        <?php
-        include 'header.php';
-        include 'TextoInicial.php';
-        include 'footer.php';
-        ?>
-    </body>
 </html>
