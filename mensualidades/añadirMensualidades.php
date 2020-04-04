09e244a6229096b13df2e98079b6e1e2ef8aa97e
Sergio2697sm <sergiomartinez2m@gmail.com>
2020-04-04 18:28:34 +0200

    modificado diseño movil

--- mensualidades/añadirMensualidades.php
+++ mensualidades/añadirMensualidades.php
@@ -3,8 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" media=" only screen and (min-device-width : 320px) and (max-device-width : 600px)" href="../css/estilos_art_movil.css">
+    <link rel="stylesheet" media=" only screen and (min-device-width : 601px) and (max-device-width : 1280px)" href="../css/estilos_art_tablet.css">
+    <link rel="stylesheet" media=" only screen and (min-device-width : 1281px) " href="../css/estilos_art.css">
     <title>Añadir Clientes</title>
-    <link rel="stylesheet" href="../css/estilos_art.css">
 </head>
 <body>
     <?php
